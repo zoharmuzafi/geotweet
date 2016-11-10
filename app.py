3fe12152e66e51ed352b748f0b1b74aab87734b7
Zohar Muzafi <zohar.muzafi@gmail.com>
2016-11-10 15:13:42 -0800

    added requirements, threading and data validation on the server and client

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ from textblob import TextBlob
 import json
 import logging
 import os
+import threading
 
 app = Flask(__name__)
 
@@ -34,8 +35,10 @@ class Tweet(object):
         self.lon = geo[1]
         self.score = score
 
+# handele the twitter streaming
 class StdOutListener(StreamListener):
     def on_data(self, data):
+        print data
         # convert returned data to json
         json_data = json.loads(data)
         # store only data with geo point
@@ -60,51 +63,84 @@ class StdOutListener(StreamListener):
     def on_error(self, status):
         print "error", status
 
-#handles Twitter authetification and the connection to Twitter Streaming API
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-stream = Stream(auth, StdOutListener())
-stream.filter(locations=[-180,-90,180,90])
-
 
 # es query for tweets in specific location
-def search_query(latitude, longitude, distance):
-    return json.dumps({
-            "size": 250,
-            "sort" : [
-                        { "score" : {"order" : "desc"}}
-                    ],
-            "query": 
-                { "bool" : 
-                    { "must" : 
-                        {"match_all" : {}
-                    }, "filter" : 
-                        {"geo_distance" : 
-                            {"distance" : 
-                                "{distance}km".format(distance=distance), 
-                                "location" : 
-                                    "{latitude}, {longitude}"
-                                    .format(latitude=latitude, 
-                                        longitude=longitude)
+def search_query(latitude, longitude, distance, search_key=''):
+    if search_key:
+        return json.dumps({
+                "size": 250,
+                "sort" : [
+                            { "score" : {"order" : "desc"}}
+                        ],
+                "query": 
+                    { "bool" : 
+                        { "must" : 
+                            { "match": {"text": "{search_key}".format(search_key=search_key)}
+                        }, "filter" : 
+                            {"geo_distance" : 
+                                {"distance" : 
+                                    "{distance}km".format(distance=distance), 
+                                    "location" : 
+                                        "{latitude}, {longitude}"
+                                        .format(latitude=latitude, 
+                                            longitude=longitude)
+                                }
                             }
                         }
                     }
-                }
-            })
+                })
+    else:
+        return json.dumps({
+                "size": 250,
+                "sort" : [
+                            { "score" : {"order" : "desc"}}
+                        ],
+                "query": 
+                    { "bool" : 
+                        { "must" : 
+                            { "match_all": {}
+                        }, "filter" : 
+                            {"geo_distance" : 
+                                {"distance" : 
+                                    "{distance}km".format(distance=distance), 
+                                    "location" : 
+                                        "{latitude}, {longitude}"
+                                        .format(latitude=latitude, 
+                                            longitude=longitude)
+                                }
+                            }
+                        }
+                    }
+                })
+
+
+#handles Twitter authetification and the connection to Twitter Streaming API
+def twiteer():
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    stream = Stream(auth, StdOutListener())
+    stream.filter(locations=[-180,-90,180,90])
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        latitude = request.form['lat']
-        longitude = request.form['long']
-        distance = request.form['distance']
-        res = es.search(index="tweets", doc_type="tweet", body=search_query(latitude, longitude, distance))
-        data = [data_point[u'_source'][u'text'] for data_point in res[u'hits'][u'hits']]
-        return render_template('index.html', data=data)
+            latitude = request.form['lat']
+            longitude = request.form['long']
+            distance = request.form['distance']
+            search_key = request.form['search_key']
+            if int(latitude) in range(-90,90) and int(longitude) in range(-180,180) and distance > 0:
+                res = es.search(index="tweets", doc_type="tweet", body=search_query(latitude, longitude, distance, search_key))
+                data = [data_point[u'_source'][u'text'] for data_point in res[u'hits'][u'hits']]
+                return render_template('index.html', data=data, message='success')
+            else:
+                return render_template('index.html', message='invalid values')
     return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
-  
+    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
+    twitter = threading.Thread(name='twiteer', target=twiteer)
+    form_post = threading.Thread(target=index)
+    twiteer().start()
+    index().start()
