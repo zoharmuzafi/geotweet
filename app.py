85b12c92c98f56862ce82a6aaee570e3f303956b
Zohar Muzafi <zohar.muzafi@gmail.com>
2016-11-09 20:36:16 -0800

    added form to the main page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # import from flask
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 # Import methods for tweepy
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
@@ -50,8 +50,11 @@ class StdOutListener(StreamListener):
         print "error", status
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        latitude = request.form['lat']
+        longitude = request.form['long']
     return render_template('index.html')
 
 if __name__ == '__main__':
